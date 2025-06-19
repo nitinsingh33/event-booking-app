8fc81dd1fd2266ec3e74143dab81f5b26abf3a80
Nitin Singh <nitin_2312res424@iitp.ac.in>
2025-06-19 21:13:08 +0530

    home.jsx

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+// src/pages/Home.jsx
+import EventCard from '../components/EventCard';
+
+const Home = () => {
+    const events = [
+        {
+            id: 1,
+            title: "IIT Patna Hackathon",
+            date: "25 June 2025",
+            description: "48-hour coding sprint with exciting"    
+        }
+    ]
+}
