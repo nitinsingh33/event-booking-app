ee52e0e9dc55cfa2ec8413eafabc14a5544b3b0c
Nitin Singh <nitin_2312res424@iitp.ac.in>
2025-06-20 11:18:07 +0530

    Day-1 homepage ui with event cards

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -7,27 +7,27 @@ const Home = () => {
             id: 1,
             title: "IIT Patna Hackathon",
             date: "25 June 2025",
-            description: "48-hour coding sprint with exciting"    
+            description: "48-hour coding sprint with exciting",   
         },
         {
             id: 2,
             title: "Tech Talk: AI & Future",
             date: "5 July 2025",
-            description: "Industry leaders share insights on AI trends"
+            description: "Industry leaders share insights on AI trends",
         },
         {
             id: 3,
             title: "Startup Pitch Fest",
-            date: "12 August",
+            date: "12 August 2025",
             description: "Pitch your startup idea to real investors.",
-        }
-    ]
+        },
+    ];
 
     return (
         <div className="p-6 bg-gray-100 min-h-screen">
-            <h1 className='text-3xl font-bold mb-6 text-center'>Upcoming Events</h1>
+            <h1 className="text-3xl font-bold mb-6 text-center">Upcoming Events</h1>
             <div classname="grid grid-cols-1 md:grid-cols-3 gap-4">
-                {events.map((event) => (
+              {events.map((event) => (
                     <EventCard key={event.id} event={event} />
                 ))}
             </div>
