51d51d045bf011c055314b8184a604bed206cd9d
Nitin Singh <nitin_2312res424@iitp.ac.in>
2025-07-03 13:56:37 +0530

    comit

--- server/index.js
+++ server/index.js
@@ -25,7 +25,10 @@ app.use("/api/test", testRoute);
 
 // ✅ DATABASE + SERVER
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("✅ Connected to MongoDB");
     app.listen(PORT, () =>
