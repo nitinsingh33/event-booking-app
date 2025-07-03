2162996ff0530b17eb18b978d1fe5ba6258a3cd9
Nitin Singh <nitin_2312res424@iitp.ac.in>
2025-07-03 18:47:16 +0530

    User.js optimise

--- server/models/User.js
+++ server/models/User.js
@@ -1,13 +1,21 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-const userSchema = new mongoose.Schema(
-    {
-        name: { type: String, required: true },
-        email: { type: String, required: true, unique: true },
-        password: { type: String, required: true },
-    },
-    { timestamps: true }
-);
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  }
+}, { timestamps: true });
 
-const User = mongoose.model("User", userSchema);
-export default User;
+export default mongoose.model('User', userSchema);
